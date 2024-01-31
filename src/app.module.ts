016b0852c494de70dfa0e52ca8314bc1a3e81f20
SUKHCHAIN SINGH <69636866+Sukhchain25@users.noreply.github.com>
2024-01-31 16:28:33 +0530

    Revert "feat: user module get post api"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    UserModule,
-    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
-    MongooseModule.forRoot(process.env.DB_URI),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/dto/create-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Gender } from '../schema/user.schema';
-
-export class CreateUserDto {
-  readonly Name: string;
-  readonly Email: string;
-  readonly Password: string;
-  readonly Phone: number;
-  readonly Gender: Gender;
-}
